8a75dc2a0450558203c625edbfc7c306fb3eef9f
luizwfelipe <luizwfelipef@gmail.com>
2024-03-25 14:10:41 -0300

    banco

---
+++ projetoMercado/mercadinho.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE mercadinho;
+USE mercadinho;
+
+CREATE TABLE usuario (
+idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(110) NOT NULL,
+login VARCHAR(110) NOT NULL UNIQUE,
+senha VARCHAR(110) NOT NULL,
+telefone VARCHAR(11) NOT NULL,
+endereco VARCHAR(110) NOT NULL,
+acesso VARCHAR(20) default 'usuario'
+);
+
+CREATE TABLE produto (
+idProduto INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(110) NOT NULL,
+descricao TEXT,
+preco FLOAT (10,2) NOT NULL,
+estoque INT NOT NULL,
+idCategoria INT,
+FOREIGN KEY (idCategoria) REFERENCES categoria (idCategoria)
+);
+
+CREATE TABLE admin (
+idAdmin INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(110) NOT NULL,
+login VARCHAR(110) NOT NULL UNIQUE,
+senha VARCHAR(110) NOT NULL );
+
+CREATE TABLE categoria (
+idCategoria INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(110) NOT NULL );
+
+CREATE TABLE venda (
+idVenda INT AUTO_INCREMENT PRIMARY KEY,
+dataVenda DATETIME,
+precoTotal FLOAT (10,2),
+quantidade INT NOT NULL,
+idUsuario INT,
+idProduto INT,
+
+FOREIGN KEY (idUsuario) REFERENCES usuario (idUsuario),
+FOREIGN KEY (idProduto) REFERENCES produto (idProduto) );
+
+CREATE TABLE backupUsuario (
+idBackup INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(110) NOT NULL,
+login VARCHAR(110) NOT NULL,
+senha VARCHAR(110) NOT NULL,
+telefone VARCHAR(11) NOT NULL,
+endereco VARCHAR(110) NOT NULL,
+exclusaoUsuario DATETIME NOT NULL);
